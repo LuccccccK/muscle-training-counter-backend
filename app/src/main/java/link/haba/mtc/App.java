295a4bbecc4cce94651c0cc61096c5fbab741569
lucccccck <oo.at.sw.tt@gmail.com>
2022-07-11 17:51:39 +0900

    first commit

---
+++ app/src/main/java/link/haba/mtc/App.java
@@ -0,0 +1,13 @@
+package link.haba.mtc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
