c02c6b16237603ce10bf863fac52f35c025c1b8f
lucccccck <oo.at.sw.tt@gmail.com>
2022-07-12 10:19:01 +0900

    Lambda関数として挙動するhandlerを仮実装

---
+++ app/src/main/java/link/haba/mtc/Sample.java
@@ -0,0 +1,32 @@
+package link.haba.mtc;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import link.haba.mtc.Sample.Input;
+import link.haba.mtc.Sample.Output;
+
+public class Sample implements RequestHandler<Input, Output> {
+
+  @Override
+  public Output handleRequest(Input in, Context context) {
+
+    final Output out = new Output();
+    out.in = in;
+    out.fullName = in.firstName + "_" + in.lastName;
+
+    return out;
+  }
+
+  public static class Input {
+    public String firstName;
+    public String lastName;
+  }
+
+  public static class Output {
+    public Input in;
+    public String fullName;
+
+  }
+
+}
